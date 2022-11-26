f8ff02d8111f82c68c2174c1f1f8f376996d9620
BharatXCloutflow <bharat.bhalla@cloutflow.com>
2022-11-26 23:55:27 +0530

    Init Graphql Nodejs

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
+            type RootMutation {
+                createEvent(name: String): String
+            }
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+		rootValue: {
+			events: () => {
+				return ["kmkmf", "kfmkfm"];
+			},
+			createEvent: (args) => {
+				const evenName = args?.name;
+
+				return evenName;
+			},
+		},
+		graphiql: true,
+	}),
+);
+
+app.listen(3000);
