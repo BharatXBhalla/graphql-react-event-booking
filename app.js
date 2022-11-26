4d2ec9ff26871f750d0321e5ddb98c92f2214964
BharatXCloutflow <bharat.bhalla@cloutflow.com>
2022-11-27 00:17:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,32 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.use(
 	"/graphql",
 	graphqlHTTP({
 		schema: buildSchema(`
+
+            type Event {
+                _id: ID!,
+                title: String!,
+                description: String!,
+                price: Float!,
+                date: String!
+            }
+
+            input EventInput{
+                title: String!,
+                description: String!,
+                price: Float!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
             type RootMutation {
-                createEvent(name: String): String
+                createEvent(eventInput: EventInput): Event
             }
             schema {
                 query: RootQuery
@@ -24,12 +41,22 @@ app.use(
         `),
 		rootValue: {
 			events: () => {
-				return ["kmkmf", "kfmkfm"];
+				return events;
 			},
 			createEvent: (args) => {
-				const evenName = args?.name;
+				const event = {
+					_id: Math.random().toString(),
+					title: args.eventInput.title,
+					description: args.eventInput.description,
+					price: args.eventInput.price,
+					date: new Date().toISOString(),
+				};
 
-				return evenName;
+				events.push(event);
+
+				console.log(event);
+
+				return event;
 			},
 		},
 		graphiql: true,
