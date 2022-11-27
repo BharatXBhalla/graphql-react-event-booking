69bf7fc3b82d7fbbfa9aa6d5d39a4486fcc25721
BharatXCloutflow <bharat.bhalla@cloutflow.com>
2022-11-27 10:10:58 +0530

    Added User

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const EventModel = require("./models/events");
+const UserModel = require("./models/user");
 
 const app = express();
 
@@ -22,10 +24,21 @@ app.use(
                 date: String!
             }
 
+            type User {
+                _id: ID!,
+                email: String!,
+                password: String,
+            }
+
             input EventInput{
                 title: String!,
                 description: String!,
                 price: Float!
+            }
+
+            input UserInput {
+                email: String!,
+                password: String!
             }
 
             type RootQuery {
@@ -33,6 +46,7 @@ app.use(
             }
             type RootMutation {
                 createEvent(eventInput: EventInput): Event
+                createUser(userInput: UserInput): User
             }
             schema {
                 query: RootQuery
@@ -49,9 +63,39 @@ app.use(
 					description: args.eventInput.description,
 					price: args.eventInput.price,
 					date: new Date(),
+					creator: "6382e77bf3cc8ec2f360533f",
 				});
 
+				const user = await UserModel.findById("6382e77bf3cc8ec2f360533f");
+
+				if (!user) {
+					throw new Error("No User Found");
+				}
+
+				user.createdEvents.push(event._id);
+
+				await user.save();
+
 				return event;
+			},
+			createUser: async (args) => {
+				const existingUser = await UserModel.findOne({
+					email: args.userInput.email,
+				});
+
+				if (existingUser) {
+					throw new Error("Existing User");
+				}
+				const hashPassword = await bcrypt.hash(args.userInput.password, 12);
+				const user = await UserModel.create({
+					email: args.userInput.email,
+					password: hashPassword,
+				});
+
+				return {
+					email: user.email,
+					_id: user._id,
+				};
 			},
 		},
 		graphiql: true,
